e4e1f696c7e232abe36b1f2dc037260ba78e7d35
Travis Wyatt <travis@juul.com>
2023-05-02 11:17:51 -0700

    Add another working SNAPSHOT and comment describing the SNAPSHOTs

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -26,8 +26,17 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(project(":library"))
+
+                /*
+                 * | SNAPSHOT               | Status | Maven group defined in...       | gradle-maven-publish-plugin version |
+                 * |------------------------|--------|---------------------------------|-------------------------------------|
+                 * | 6.4.1-3c7da21-SNAPSHOT | Broken | `gradle.properties` via `GROUP` | 0.25.2                              |
+                 * | 6.4.1-f1d2a0d-SNAPSHOT | Works  | `build.gradle.kts` via `group`  | 0.25.2                              |
+                 * | 6.4.1-51793c4-SNAPSHOT | Works  | `gradle.properties` via `GROUP` | 0.22.0                              |
+                 */
                 implementation("com.juul.tuulbox:encoding:6.4.1-f1d2a0d-SNAPSHOT") // Broken
 //                implementation("com.juul.tuulbox:encoding:6.4.1-3c7da21-SNAPSHOT") // Working
+//                implementation("com.juul.tuulbox:encoding:6.4.1-51793c4-SNAPSHOT") // Working
             }
         }
     }
